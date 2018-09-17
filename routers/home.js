32ed9fbfe4d48b089ff95a2a92ccb9f9b9dfcf39
yanlele <331393627@qq.com>
2018-09-17 13:47:31 +0800

    [+]添加示例文件

--- routers/home.js
+++ routers/home.js
@@ -11,8 +11,15 @@ router.get('/', (ctx) => {
         name: 'yanle',
         age: 35
     };
-    ctx.logger.debug('return object', JSON.stringify(returnObject));
-    ctx.logger.debug(JSON.stringify(ctx.query));
+    // ctx.logger.debug('request Param: ',JSON.stringify(ctx.query));
+    // ctx.logger.debug('response: ', JSON.stringify(returnObject));
+    ctx.logger.info('koa-logs-middleware');
+    ctx.logger.error('koa-logs-middleware');
+    ctx.logger.success('koa-logs-middleware');
+    ctx.logger.trace('koa-logs-middleware');
+    ctx.logger.debug('koa-logs-middleware');
+    ctx.logger.warn('koa-logs-middleware');
+    ctx.logger.fatal('koa-logs-middleware');
     ctx.body = returnObject;
 });
 
