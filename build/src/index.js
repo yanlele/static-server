05323939e51f8bde8e6d8a25a0bfce3d927a8782
yanlele <331393627@qq.com>
2018-09-23 12:59:11 +0800

    [+]提交umi+dva项目的前端开发结构

--- build/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-ReactDom.render(
-    <div>hello react!</div>,
-    document.getElementById('app')
-);
---
+++ build/src/pages/index/index.js
@@ -0,0 +1,24 @@
+import styles from './index.less';
+import {Card, Row, Col} from 'antd';
+
+export default function () {
+  return (
+    <div className={styles.home_background_img}>
+        <Row className="pt15_ratio">
+            <Col span={8} offset={8}>
+                <Card
+                    title="请登录"
+                    style={{ width: '100%',
+                        background: 'inherit'
+                    }}
+                >
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+            </Col>
+        </Row>
+
+    </div>
+  );
+}
