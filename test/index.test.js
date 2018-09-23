0f806aac73855b0b2f1559fe158af78b04dfae09
yanlele <331393627@qq.com>
2018-09-23 13:54:58 +0800

    [+]添加完整测试完成

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const koa = require('../app');
+const requestKoa = require('supertest')(koa.listen(3000));
+
+
+describe('koa service', function () {
+    it('检测一个get请求', function (done) {
+        requestKoa
+            .get('/home')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                console.log(res.body);
+                expect(res.body).is.a.string;
+                done();
+            })
+    });
+})
