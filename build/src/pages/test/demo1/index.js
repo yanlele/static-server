b271a20c3c08d879fd6454ef7a24185a0a6cecd4
yanlele <331393627@qq.com>
2018-09-29 11:19:09 +0800

    [+]添加一个react-motion动画

---
+++ build/src/pages/test/demo1/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Motion, spring} from "react-motion";
+import style from './index.less';
+
+export default class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    };
+
+    handleMouseDown = () => {
+        this.setState({open: !this.state.open});
+    };
+
+    handleTouchStart = (e) => {
+        e.preventDefault();
+        this.handleMouseDown();
+    };
+
+    render() {
+        return (
+            <div>
+                <button
+                    onMouseDown={this.handleMouseDown}
+                    onTouchStart={this.handleTouchStart}>
+                    Toggle
+                </button>
+
+                <Motion style={{x: spring(this.state.open ? 400 : 0)}}>
+                    {({x}) =>
+                        <div className={`${style.demo0} ${style.demo0_box}`}>
+                            <div className={style.demo0_block} style={{
+                                WebkitTransform: `translate3d(${x}px, 0, 0)`,
+                                transform: `translate3d(${x}px, 0, 0)`,
+                            }} />
+                        </div>
+                    }
+                </Motion>
+            </div>
+        );
+    };
+}
